a289a6ecfec06cef0f2305771a74438ab417b63f
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-24 18:59:03 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM mcr.microsoft.com/dotnet/sdk/core:7.0 AS build-env
+WORKDIR /app
+
+
+COPY *.csproj ./
+RUN dotnet restore
+
+
+COPY . ./
+RUN dotnet publish -c Release -o out
+
+
+FROM mcr.microsoft.com/dotnet/aspnet:7.0
+WORKDIR /app
+EXPOSE 80
+COPY --from=build-env /app/out .
+ENTRYPOINT ["dotnet", "weatherapi.dll"]
---
+++ main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
+}
+provider "azurerm"{
+  features  {}
+}
+terraform{
+backend "azurerm"{
+    resource_group_name = "mahaksiacrg"
+    storage_account_name = "mahaksiacstorageaccount"
+    container_name = "mahaksiaccontainer"
+    key = "terraform.tfstate"
+}
+}
+
+resource "azurerm_resource_group" "resourcegroup" {
+    name = "mahaksiacrg"
+    location = "west us"
+  
+}
+resource "azurerm_container_registry" "azurecontainerregistry" {
+  name                     = "mahaksiacacr"
+  resource_group_name      = azurerm_resource_group.resourcegroup.name
+  location                 = azurerm_resource_group.resourcegroup.location
+  sku                      = "Standard"
+  admin_enabled            = true
+}
+
+resource "azurerm_app_service_plan" "serviceplan"{
+    name = "mahaksiacaserviceplan"
+    location = azurerm_resource_group.resourcegroup.location
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+
+    sku {
+      tier = "Standard"
+      size = "S1"
+    }
+  
+}
+resource "azurerm_app_service" "appservice" {
+    name = "mahaksiacappservice"
+    location = azurerm_resource_group.resourcegroup.location
+    resource_group_name = azurerm_resource_group.resourcegroup.name
+    app_service_plan_id = azurerm_app_service_plan.serviceplan.id
+  site_config {
+    dotnet_framework_version = "v7.0"
+    always_on                = true
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "DOCKER_REGISTRY_SERVER_URL"         = azurerm_container_registry.azurecontainerregistry.login_server
+    "DOCKER_REGISTRY_SERVER_USERNAME"    = azurerm_container_registry.azurecontainerregistry.admin_username
+    "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.azurecontainerregistry.admin_password
+    "WEBSITES_PORT"                      = "80"
+  }
+}
