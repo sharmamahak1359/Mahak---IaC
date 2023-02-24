829fa419cf27aa694f1adb62b6e6241db6c1e0c7
Mahak Sharma <sharmamahak1359@gmail.com>
2023-02-24 23:22:37 +0530

    new

--- main.tf
+++ main.tf
@@ -47,9 +47,9 @@ resource "azurerm_app_service" "appservice" {
     location = azurerm_resource_group.resourcegroup.location
     resource_group_name = azurerm_resource_group.resourcegroup.name
     app_service_plan_id = azurerm_app_service_plan.serviceplan.id
-  site_config {
-    dotnet_framework_version = "v5.0"
-    always_on                = true
+  //site_config {
+    //dotnet_framework_version = "v5.0"
+    //always_on                = true
   }
 
   app_settings = {
@@ -59,4 +59,4 @@ resource "azurerm_app_service" "appservice" {
     "DOCKER_REGISTRY_SERVER_PASSWORD"    = azurerm_container_registry.azurecontainerregistry.admin_password
     "WEBSITES_PORT"                      = "80"
   }
-}
+
